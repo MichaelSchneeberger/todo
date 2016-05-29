5469d590d064c67a1be3439d449e00da07ee604e
Michael Schneeberger <michael.schneeb@gmail.com>
2016-05-29 22:30:05 +0200

    todos can be prosponed and marked as done

--- urls.py
+++ urls.py
@@ -6,8 +6,16 @@ app_name = 'todo'
 urlpatterns = [
     # ex: /todo/overview/
     url(r'^overview/$', views.overview),
+
+    # ex: /todo/set_todo_as_done/
+    url(r'^set_todo_as_done/$', views.set_todo_as_done, name='set_todo_as_done'),
+
+    # ex: /todo/prostpone_date/
+    url(r'^prostpone_date/$', views.prostpone_date, name='prostpone_date'),
+
     # ex: /todo/alltodo/
     url(r'^alltodo/$', views.alltodo),
+
 	# ex: /todo/addtodo/
     url(r'^addtodo/$', views.addtodo, name='addtodo'),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.utils import timezone
@@ -14,38 +16,68 @@ def alltodo(request):
 
 def overview(request):
     open_todo_list = HaveToTask.objects.filter(done_date__isnull=True)
-    soft_todo_list = open_todo_list.filter( \
+    soft_todo = open_todo_list.filter( \
         Q(start_date__lte=timezone.now()) | Q(start_date__isnull=True), \
-    )
+    ).order_by('soft_due_date').first()
     hard_todo_list = open_todo_list.filter( \
         soft_due_date__lte=timezone.now(), hard_due_date__isnull=False
     )
-    context = {'soft_todo_list': soft_todo_list,
+    context = {'soft_todo': soft_todo,
                'hard_todo_list': hard_todo_list}
     return render(request, 'todo/overview.html', context)
+
+def prostpone_date(request):
+    try:
+        todo_id = request.POST['todo_id']
+        prospone_date = _convert_input_to_date(request.POST['prospone_date'])
+        todo = HaveToTask.objects.get(pk=todo_id)
+        todo.soft_due_date = prospone_date
+        todo.save()
+    except:
+        pass
+    return HttpResponseRedirect('../overview/')
+
+def set_todo_as_done(request):
+    try:
+        todo_id = request.POST['todo_id']
+        todo = HaveToTask.objects.get(pk=todo_id)
+        todo.done_date = timezone.now()
+        todo.save()
+    except:
+        pass
+    return HttpResponseRedirect('../overview/')
+
+def _convert_input_to_date(date_input):
+    format_date_list = ['%Y%m%d', '%Y%m%d %H%M']
+    format_interval_list = [r'\+(\d+)']
+    current_tz = timezone.get_current_timezone()
+    date_object = None
+    for format in format_date_list:
+        try:
+            date_object = current_tz.localize(datetime.strptime(date_input, format))
+            break
+        except:
+            pass
+
+    for format in format_interval_list:
+        try:
+            interval = int(re.match(format, date_input, re.M | re.I).group(1))
+            date_object = timezone.now() + timezone.timedelta(days=interval)
+            break
+        except:
+            pass
+
+    return date_object
 
 def addtodo(request):
     if request.method == 'POST':
         form = HaveToForm(request.POST)
         if form.is_valid():
-
-            def convert_input_to_date(date_input):
-                format_list = ['%Y%m%d', '%Y%m%d %H%M']
-                current_tz = timezone.get_current_timezone()
-                date_object = None
-                for format in format_list:
-                    try:
-                        date_object = current_tz.localize(datetime.strptime(date_input, format))
-                        break
-                    except:
-                        pass
-                return date_object
-
             have_to_task = HaveToTask(task_name=form.cleaned_data['todo_name'], \
                                       task_text=form.cleaned_data['todo_text'], \
-                                      start_date=convert_input_to_date(form.cleaned_data['start_date']), \
-                                      soft_due_date=convert_input_to_date(form.cleaned_data['soft_due_date']), \
-                                      hard_due_date=convert_input_to_date(form.cleaned_data['hard_due_date']))
+                                      start_date=_convert_input_to_date(form.cleaned_data['start_date']), \
+                                      soft_due_date=_convert_input_to_date(form.cleaned_data['soft_due_date']), \
+                                      hard_due_date=_convert_input_to_date(form.cleaned_data['hard_due_date']))
             have_to_task.save()
         else:
             print('not valid')
